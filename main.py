53b7fb9568a5450adb32078b8853a056af68691f
oyefimova <oyefimova@kse.org.ua>
2024-11-20 23:12:48 +0200

    added task2 file

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
-with open('Olympic Athletes - raw.tsv', 'r') as file:
-    line = file.readline()
-    header = next(file)
-    print(header)
-    data = []
-    for line in file:
-        line = line[:-1].split('\t')
-        data.append(line)
-print(data)
+def installer():
+    with open('Olympic Athletes - raw.tsv', 'r') as file:
+        header = next(file)
+        data = []
+        line = file.readline()
+        while line:
+            for line in file:
+                line = line[:-1].split('\t')
+                data.append(line)
+                print(line)
+            line = file.readline()
