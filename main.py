b5b6baadd1350bd4e057608c1f784a91c352e66d
ozinchuk <ozinchuk@kse.org.ua>
2024-11-21 23:36:27 +0200

    added the func to search for the data needed

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def installer():
-    with open('Olympic Athletes - raw.tsv', 'r') as file:
+import argparse
+
+def installer(source_file):
+    with open(source_file, 'r') as file:
         header = next(file)
         data = []
         line = file.readline()
@@ -7,5 +9,20 @@ def installer():
             for line in file:
                 line = line[:-1].split('\t')
                 data.append(line)
-                print(line)
             line = file.readline()
+    return data
+
+def output(data):
+    medals = {'Gold': 0, 'Silver': 0, 'Bronze': 0}
+    for i in range(10):
+        for row in data:
+            TEAM = row[6]
+            NOC = row[7]
+            MEDAL = row[14]
+            if row[9] in str(args.medals) and MEDAL != 'NA':
+                if TEAM in str(args.medals) or NOC in str(args.medals):
+                    print(f'{row[1]} - {row[12]} - {row[-1]}')
+            if TEAM in str(args.medals) or NOC in str(args.medals):
+                if MEDAL != 'NA':
+                    medals[MEDAL] += 1
+            print(f'Gold:{medals['Gold']}, Silver:{medals['Silver']}, Bronze:{medals['Bronze']}')
