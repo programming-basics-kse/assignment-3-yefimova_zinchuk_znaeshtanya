49eec290c6a20e40774db9c5dc539d89800b448d
ozinchuk <ozinchuk@kse.org.ua>
2024-11-22 00:54:40 +0200

    '-output' argument added

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import argparse
 
 
-
 def installer(source_file):
     with open(source_file, 'r') as file:
         header = next(file)
@@ -23,7 +22,8 @@ def output(data):
         MEDAL = row[14]
         if row[9] in str(args.medals) and MEDAL != 'NA':
             if TEAM in str(args.medals) or NOC in str(args.medals):
-                print(f'{row[1]} - {row[12]} - {row[-1]}')
+                result_1 = (f'{row[1]} - {row[12]} - {row[-1]}')
+                print(result_1)
                 count += 1
         if TEAM in str(args.medals) or NOC in str(args.medals):
             if MEDAL != 'NA':
@@ -31,21 +31,33 @@ def output(data):
         if count == 10:
             break
 
-        if TEAM not in str(args.medals) or NOC not in in str(args.medals):
-            print('Country not found')
+    if TEAM not in str(args.medals) and NOC not in str(args.medals):
+        print('Country not found')
 
-        if row[9] not in str(args.medals):
-            print('No Olympics were held that year')
+    if row[9] not in str(args.medals):
+        print('No Olympics were held that year')
 
 
 
-    print(f'Gold:{medals['Gold']}, Silver:{medals['Silver']}, Bronze:{medals['Bronze']}')
+    result_2 = (f'Gold:{medals["Gold"]}, Silver:{medals["Silver"]}, Bronze:{medals["Bronze"]}')
+    print(result_2)
 
+    return result_1, result_2
 
 parser = argparse.ArgumentParser()
 parser.add_argument('file', type=str, help='Choose the file')
 parser.add_argument('-medals', nargs=2, help='Medals for a team during the year')
+parser.add_argument('-output', nargs=1, help='Writing the output in the file')
 
 args = parser.parse_args()
+
+
+
 data = installer(args.file)
-output(data)
+
+
+result_1, result_2 = output(data)
+if args.output:
+    with open (args.output[0], 'w') as file:
+       file.write(result_1)
+       file.write(result_2)
